282095f588227dd39f7a97ad16cc4676eaf56773
eduardopacompialopez <eduardopacompialopez@gmail.com>
2023-03-11 18:46:45 -0500

    completo hasta el video 67

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,8 +27,9 @@ class AppServiceProvider extends ServiceProvider
             //de esta forma todas las vistas acceden a esta variable de pruebas
             View::share('prueba' , 'este es un mensaje de prueba');
 
-
-
-
+            //EN CASO QUIERA USAR BOOTSTRAP POR DEFECTO EN VES DE TAILWIND, PRIMERO EN EL layout.blade.php incluyo el css y js de javascript y leugo vengo aqui al AppServiceProvider para setear el boostrap
+            //En caso quiera usar una version especifica de bootstrap uso useBootrapFive si quiero al version 5
+            //Paginator::useBootstrap();
+            Paginator::useBootstrapFive();
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,14 @@
 
 
     <title>{{$title ?? 'RunaSystem'}}</title>  <!--En caso de que le coloque ?? significa que es para que me tome por valor de defecto RunaSystem-->
-    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- <script src="https://cdn.tailwindcss.com"></script>  en caso quiero usar tailwind uso este script -->
+    
+   
+    <!-- EN CASO QUIERA USAR BOOTSTRAP POR DEFECTO EN VES DE TAILWIND, PRIMERO EN EL layout.blade.php incluyo el css y js de javascript y leugo vengo aqui al AppServiceProvider para setear el boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> <!--ESTO EN SI EN CASO QUIERO USAR BOOTRSTRAP 5-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> 
+
+
 </head>
 <body>
 
